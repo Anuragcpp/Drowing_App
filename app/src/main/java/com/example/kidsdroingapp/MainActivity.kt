222890b06e82e1528b8b78a4c51eaa74912ec21b
Anurag Sen <anurag007sen@gmail.com>
2024-04-15 01:43:02 +0530

    Permission request setup done

--- app/src/main/java/com/example/kidsdroingapp/MainActivity.kt
+++ app/src/main/java/com/example/kidsdroingapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.kidsdroingapp
 
+import android.Manifest
 import android.app.Activity
 import android.app.AlertDialog
 import android.app.Dialog
@@ -13,6 +14,10 @@ import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.app.ActivityCompat.startActivityForResult
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
 import com.example.kidsdroingapp.databinding.ActivityMainBinding
@@ -27,9 +32,55 @@ class MainActivity : AppCompatActivity() {
     private lateinit var linearLayoutPaintColor : LinearLayout
     private lateinit var ib_brush_color : ImageButton
     private lateinit var binding: ActivityMainBinding
+    private lateinit var galleryBtn : ImageButton
 
     private val colorViewReqCode : Int = 100
     private lateinit var colorCode : String
+
+    //camera and gallery loncher
+    private val cameraAndGalleryResultLoncher : ActivityResultLauncher<Array<String>> =
+        registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()){
+
+            permissions ->
+            //2nd forEach loog
+            permissions.entries.forEach {
+                val permissionName = it.key
+                val isGranted = it.value
+
+                if (isGranted){
+                    //if permission is grante the we have to check which one is granted
+                    if (permissionName == Manifest.permission.CAMERA){
+                        Toast.makeText(this,
+                            "Permission granted for camera",
+                            Toast.LENGTH_SHORT)
+                            .show()
+                    } else if (permissionName == Manifest.permission.WRITE_EXTERNAL_STORAGE){
+                        //TODO don't know what permission is needed for pick an image form the gallery
+
+                        Toast.makeText(this,
+                            "Permission granted for external storage",
+                            Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                }else{
+                    //is permission is denied the we have to check which one is been denied
+                    if (permissionName == Manifest.permission.CAMERA){
+                        Toast.makeText(this,
+                            "Permission Denied for camera",
+                            Toast.LENGTH_SHORT)
+                            .show()
+                    } else if (permissionName == Manifest.permission.WRITE_EXTERNAL_STORAGE){
+                        //TODO don't know what permission is needed for pick an image form the gallery
+
+                        Toast.makeText(this,
+                            "Permission Denied for external storage",
+                            Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                }
+            }
+        }
 
 
 
@@ -60,10 +111,51 @@ class MainActivity : AppCompatActivity() {
             startActivityForResult(intent,colorViewReqCode)
         }
 
+        /// for the gallery btn
+        galleryBtn = binding.galleryBtn
+        galleryBtn.setOnClickListener {
+            requestStoragePermission()
+//            cameraAndGalleryResultLoncher.launch(
+//                arrayOf(
+//                    Manifest.permission.CAMERA,
+//                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+//                )
+//            )
+        }
 
 
 
 
+
+    }
+
+    private fun requestStoragePermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+            this,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )){
+            showRationalDialog("Kids drawing App", "Kids Drawing App Needs  to access your external storage")
+        }else{
+            cameraAndGalleryResultLoncher.launch(
+                arrayOf(
+                    Manifest.permission.CAMERA,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                )
+            )
+        }
+    }
+
+    private fun showRationalDialog(title : String, message : String) {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(title)
+        builder.setMessage(message)
+
+        builder.setNeutralButton("Cancle"){dialog , _->
+            dialog.dismiss()
+        }
+        builder.setPositiveButton("Ok"){dialog, _->
+            dialog.dismiss()
+        }
     }
 
 
